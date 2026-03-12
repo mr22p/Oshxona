3dbe0157e408d956f757d7e112278f87721638ca
Mahmudjon <mrfx404@gmail.com>
2026-03-12 20:21:40 +0500

    yangilanish

---
+++ Oshxona_pr/menu/tests.py
@@ -0,0 +1,43 @@
+son = int(input("Son kiriting: "))
+
+if son > 0:
+    print("Musbat son")
+elif son < 0:
+    print("Manfiy son")
+else:
+    print("Son 0 ga teng")
+
+son = int(input("Son kiriting: "))
+
+if son % 2 == 0:
+    print("Juft son")
+else:
+    print("Toq son")
+
+ball = int(input("Ball kiriting: "))
+
+if ball >= 90:
+    print("5 baho")
+elif ball >= 70:
+    print("4 baho")
+elif ball >= 50:
+    print("3 baho")
+else:
+    print("2 baho")        
+
+a = int(input("1-sonni kiriting: "))
+b = int(input("2-sonni kiriting: "))
+
+if a > b:
+    print("Katta son:", a)
+else:
+    print("Katta son:", b)
+
+yosh = int(input("Yoshingizni kiriting: "))
+
+if yosh <= 6:
+    print("Bola")
+elif yosh <= 17:
+    print("O‘quvchi")
+else:
+    print("Katta")        
