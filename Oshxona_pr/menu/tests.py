69b3b978853a17761853f7d05dc03e3691283665
Mahmudjon <mrfx404@gmail.com>
2026-03-12 21:26:44 +0500

    kodlar olindi2

--- Oshxona_pr/menu/tests.py
+++ Oshxona_pr/menu/tests.py
@@ -7,23 +7,4 @@ yigindi = 0
 for i in range(1, n + 1):
     yigindi += i
 
-print("Yig'indi:", yigindi)
-
-n = int(input("Son kiriting: "))
-faktorial = 1
-
-for i in range(1, n + 1):
-    faktorial *= i
-
-print("Faktorial:", faktorial)
-
-for i in range(1, 101):
-    if i % 2 == 0:
-        print(i)
-
-son = int(input("Son kiriting (0 chiqish uchun): "))
-
-while son != 0:
-    son = int(input("Son kiriting (0 chiqish uchun): "))
-
-print("Dastur tugadi")        
+print("Yig'indi:", yigindi) 
