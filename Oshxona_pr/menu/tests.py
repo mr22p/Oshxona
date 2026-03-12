760011393821d3b5c05fe9eb4b9532e91202fc46
Mahmudjon <mrfx404@gmail.com>
2026-03-12 21:33:00 +0500

    kodlar yozildi2

--- Oshxona_pr/menu/tests.py
+++ Oshxona_pr/menu/tests.py
@@ -1,26 +1,6 @@
 from collections import deque
 
 graph = {
-'A': ['B','C'],
-'B': ['D','E'],
-'C': ['F'],
-'D': [],
-'E': [],
-'F': []
-}
-
-visited = []
-queue = deque(['A'])
-
-while queue:
-    node = queue.popleft()
-    if node not in visited:
-        print(node)
-        visited.append(node)
-        queue.extend(graph[node])
-
-
-graph = {
 'A':['B','C'],
 'B':['D','E'],
 'C':['F'],
