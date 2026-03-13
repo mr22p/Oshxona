99be0b39e730b0b9dde3358bf4a3c74a145b904a
Mahmudjon <mrfx404@gmail.com>
2026-03-13 07:28:56 +0500

    yangi

---
+++ Oshxona_pr/menu/tests.py
@@ -0,0 +1,7 @@
+isitma = input("Isitma bormi? (ha/yoq): ")
+yotal = input("Yo'tal bormi? (ha/yoq): ")
+
+if isitma == "ha" and yotal == "ha":
+    print("Sizda gripp ehtimoli bor")
+else:
+    print("Aniq tashxis uchun shifokorga murojaat qiling")
