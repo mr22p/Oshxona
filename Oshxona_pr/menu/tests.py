75206847dc627eed1ad40d0cdbaef94bd8ca26f7
Mahmudjon <mrfx404@gmail.com>
2026-03-14 06:09:58 +0500

    Yangi

---
+++ Oshxona_pr/menu/tests.py
@@ -0,0 +1,7 @@
+age = 15
+grade_year = 2011
+print(f"Siz {age} yoshdasiz va {grade_year} yilda o'qishni boshlaysiz.")
+
+yosh = 20
+grade_year = 2006
+print(f"Siz {yosh} yoshdasiz va {grade_year} yilda o'qishni boshlaysiz.")
